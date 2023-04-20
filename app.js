aa82d3a69ac8f6c82d473f11ea5b6bbcbe839dd8
hyojinsim90 <44770462+hyojinsim90@users.noreply.github.com>
2023-04-20 21:55:33 +0900

    3.1 Form Submission
    html 에 form 도입

---
+++ app.js
@@ -0,0 +1,11 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    const username = loginInput.value;
+
+
+}
+
+
+loginButton.addEventListener("click", onLoginBtnClick);
